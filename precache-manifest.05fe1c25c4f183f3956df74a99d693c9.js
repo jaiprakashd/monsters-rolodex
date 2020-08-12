11f1bb6a3b072280eefa3975fcc5d4970ae19ad3
jaiprakash <jaiprakash@users.noreply.github.com>
2020-08-12 16:48:41 +0530

    Updates

---
+++ precache-manifest.05fe1c25c4f183f3956df74a99d693c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3609f1679502afa3fee213c888e91b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "821ae3b54e4f1e802c61",
+    "url": "/monsters-rolodex/static/css/main.7f44a6d5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821ae3b54e4f1e802c61",
+    "url": "/monsters-rolodex/static/js/main.5da5301d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
